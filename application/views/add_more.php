86a9fcfea5723634aaa9fbc30ed4fe2214c4b1b6
sanitmca03 <sanitbca03@gmail.com>
2018-03-13 18:48:33 +0530

    update
    update

--- application/views/add_more.php
+++ application/views/add_more.php
@@ -3,7 +3,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ADD - REMOVE</title>
+        <title>ADD - REMOVE - Sanit</title>
         <link href="<?php echo base_url('assests/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
         <link href="<?php echo base_url('assests/datatables/css/dataTables.bootstrap.css') ?>" rel="stylesheet">
         <style>
